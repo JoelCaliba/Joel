91b854af600dba4ff6cec466c2c6549bdd19922c
Dimas Ávila <dimasavilamartinez@gmail.com>
2022-11-21 16:24:49 +0100

    Ejercicio resuleto

---
+++ datos.py
@@ -0,0 +1,16 @@
+from random import randint
+titulo = "DNI,Nombre y Apellido,Direccion,Telefono,Correo electronico,Nota_parcial"
+
+
+alumnos = [
+["44696178","Joel Caliba", "Urquiza1898", "3875458198", "joel@gmail.com", randint(0,10)],
+["32456486","Maria Lopez","Tucuman 1176","3875234212","marialopez@gmail.com",randint(0,10)],
+["35382123","Pablo Torres","Caseros 1896","3874676421","pablotorres@gmail.com",randint(0,10)],
+["40754345","Carlos Gomez","Junin 1073","3874076245","carlosgomez@gmail.com",randint(0,10)],
+["27534746","Cristian Fernandez","San Luis 135","3874347686","cristianfernandez@gmail.com",randint(0,10)],
+["459711748","Facundo Torres","Ayacucho 429","387574938","facundo@gmail.com",randint(0,10)],
+["354712721","Nahuel Tolaba","San Luis 52","3875128527","nahuel@gmail.com",randint(0,10)],
+["29325693","Martina Palma","España 308","3876248628","martina@gmail.com",randint(0,10)],
+["31534631","Matias Cruz","Av. Belgrano 2152","3876688241","matias@gmail.com",randint(0,10)],
+["44212745","Francisco Gutierrez","12 de Octubre 1257","3875981283","francisco@gmail.com",randint(0,10)]
+]
